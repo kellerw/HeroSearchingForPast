6914f80ba534ee72df2694aa454819e8d7ec07e0
W <tkeller34@gatech.edu>
2018-10-25 19:40:12 -0400

    Gate opens from top

--- Model/CastleGate.java
+++ Model/CastleGate.java
@@ -1,6 +1,7 @@
 public class CastleGate extends Base
 {
 	private boolean stepped = false;
+	private boolean open = false;
 	public CastleGate()
 	{
 		setSprite("Gate.png");
@@ -15,6 +16,8 @@ public class CastleGate extends Base
 	}
 	public boolean isWalkable(Interactor t)
 	{
+		if(open)
+			return true;
 		for(int i = 0; i < GameWorld.getWorld().getWide(); i++)
 			for(int j = 0; j < GameWorld.getWorld().getHigh(); j++)
 			{
@@ -26,6 +29,13 @@ public class CastleGate extends Base
 						return false;
 			}
 		setSprite("GateOpen.png");
+		open = true;
+		return true;
+	}
+	public boolean isWalkableDown(Interactor i)
+	{
+		setSprite("GateOpen.png");
+		open = true;
 		return true;
 	}
 }
